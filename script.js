66cae93c8c24b728d4080b74853b69394539fa77
Marie Gadda <mariegadda@gmail.com>
2017-04-06 19:10:02 -0400

    basic query url

--- script.js
+++ script.js
@@ -5,26 +5,26 @@ $("#submit").click(function() {
     event.preventDefault();
        
 
-		var ingredients = $("#textArea").val().trim();
-		console.log(ingredients);
+		// var ingredients = $("#textArea").val().trim();
+		// console.log(ingredients);
 
-      //   var queryURL = 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/searchComplex?addRecipeInformation=false&cuisine=american&excludeIngredients=coconut%2C+mango&fillIngredients=false&includeIngredients=onions%2C+lettuce%2C+tomato&instructionsRequired=false&intolerances=peanut%2C+shellfish&limitLicense=false&maxCalories=1500&maxCarbs=100&maxFat=100&maxProtein=100&minCalories=150&minCarbs=5&minFat=5&minProtein=5&number=10&offset=0&query=burger&ranking=1&type=main+course';
+        var queryURL = 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients=carrots%2Conion%2Ckale&limitLicense=false&number=5&ranking=1';
 
  
        
-      //   $.ajax({
-      //     url: queryURL,
-      //     method: "GET",
-      //     headers: {
-      //     	'X-Mashape-Key': 'N6Iqu17EWCmshbX4K3FDHGcK8Zocp1Qy6mljsnMMwv92cEfZrE',
-  		 	// 'Accept': 'application/json'
-   		 //  }
+        $.ajax({
+          url: queryURL,
+          method: "GET",
+          headers: {
+          	'X-Mashape-Key': 'N6Iqu17EWCmshbX4K3FDHGcK8Zocp1Qy6mljsnMMwv92cEfZrE',
+  		 	'Accept': 'application/json'
+   		  }
 
-      //   }).done(function(response) {
-      //   	var title = response.results[i].title;
-      //     $("#recipes-here").html(title);
-      //     console.log(response);
-      //   });
+        }).done(function(response) {
+        	// var title = response.results[i].title;
+         //  $("#recipes-here").html(title);
+          console.log(response);
+        });
 
     });
 
